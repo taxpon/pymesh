707d704d108f591305830e520d27db9200c1d2df
Takuro Wada <taxpon@gmail.com>
2015-10-16 16:32:48 +0900

    Add function to handle empty data

--- example/sample.py
+++ example/sample.py
@@ -4,6 +4,9 @@ from pymesh import obj
 
 
 def main():
+    print(stl.__file__)
+    empty = stl.Stl()
+    e2 = obj.Obj()
     m = stl.Stl('sample.stl')
     m2 = obj.Obj('sample.obj')
     print(m.get_volume())
@@ -12,7 +15,10 @@ def main():
     m.rotate_y(30)
     m.translate_x(2)
     m.join(m2)
+    empty.join(m2)
+    empty.join(e2)
     m.save_stl("sample_out.stl", update_normals=True)
+    empty.save_stl("empty.stl")
 
 
 if __name__ == '__main__':
--- pymesh/__init__.py
+++ pymesh/__init__.py
@@ -5,7 +5,7 @@
 #
 
 __title__ = "pymesh"
-__versioninfo__ = (1, 0, 1)
+__versioninfo__ = (1, 0, 2)
 __version__ = ".".join(map(str, __versioninfo__))
 __author__ = "Takuro Wada"
 __license__ = "MIT"
